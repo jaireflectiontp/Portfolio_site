b4f937ac6fc116870294c9a439ded6296bb18a7c
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-08-23 20:54:34 +0530

    Project component created

---
+++ my_portfolio/src/components/layouts/Projects.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import "../../styles/_main.scss";
+import "../../styles/_mediaQuery.scss";
+import HeadingTemp from '../common/HeadingTemp';
+import ProjectCard from '../common/ProjectCard';
+const Projects = () => {
+    return (
+        <>
+            <section className="projects-section">
+                <HeadingTemp headingNumber="03." headingText="Projects" />
+                <div className="projects-section-in">
+                    <ProjectCard
+                        projectTitle="Spotigy"
+                        projectDescription="lorem Functional Programming is another popular approach to software development. In Functional Programming, code is organized into smaller, basic functions that can be combined to build complex programs. "
+                        tech1="edd"
+                        tech2="erer"
+                        tech3="ffvvf"
+                        tech4="vvvv"
+                        tech5=""
+                        tech6=""
+                        gitLink="#"
+                        deployLink="#"
+                        projectImg=""
+                    />
+                    <ProjectCard
+                        projectTitle="Spotigy"
+                        projectDescription="lorem Functional Programming is another popular approach to software development. In Functional Programming, code is organized into smaller, basic functions that can be combined to build complex programs. "
+                        tech1="edd"
+                        tech2="erer"
+                        tech3="ffvvf"
+                        tech4="vvvv"
+                        tech5=""
+                        tech6=""
+                        gitLink="#"
+                        deployLink="#"
+                        projectImg=""
+                    />
+                    <ProjectCard
+                        projectTitle="Spotigy"
+                        projectDescription="lorem Functional Programming is another popular approach to software development. In Functional Programming, code is organized into smaller, basic functions that can be combined to build complex programs. "
+                        tech1="edd"
+                        tech2="erer"
+                        tech3="ffvvf"
+                        tech4="vvvv"
+                        tech5=""
+                        tech6=""
+                        gitLink="#"
+                        deployLink="#"
+                        projectImg=""
+                    />
+                    <ProjectCard
+                        projectTitle="Spotigy"
+                        projectDescription="lorem Functional Programming is another popular approach to software development. In Functional Programming, code is organized into smaller, basic functions that can be combined to build complex programs. "
+                        tech1="edd"
+                        tech2="erer"
+                        tech3="ffvvf"
+                        tech4="vvvv"
+                        tech5=""
+                        tech6=""
+                        gitLink="#"
+                        deployLink="#"
+                        projectImg=""
+                    />
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Projects
