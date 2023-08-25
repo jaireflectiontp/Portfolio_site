ae0c89354d6b938183b9153829343dc9aed30ad8
jai.reflectiontp <jai.reflectiontp@gmail.com>
2023-08-25 12:09:12 +0530

    styles implemented

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,9 +1,21 @@
-
-import './App.css';
+import './App.scss';
+import Header from './components/common/Header';
+import About from './components/layouts/About';
+import Contact from './components/layouts/Contact';
+import Education from './components/layouts/Education';
+import Home from './components/layouts/Home';
+import Projects from './components/layouts/Projects';
 function App() {
   return (
     <div className="App">
-      hii
+      <header><Header /></header>
+      <main>
+        <Home />
+        <About />
+        <Education />
+        <Projects />
+        <Contact />
+      </main>
     </div>
   );
 }
